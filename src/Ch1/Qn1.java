7c68ec30082ee249a74391723a809bd43963ecc6
Gnana <cgp14697@gmail.com>
2018-07-21 22:04:59 +0530

    CTCI

--- src/Ch1/Qn1.java
+++ src/Ch1/Qn1.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Ch1;
 import java.util.*;
 import java.io.*;
-/**
- *
- * @author Pavan
- */
 public class Qn1 {
     public static void main(String args[])
     {
